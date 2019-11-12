935e048c0b10dde058abb483a86aa5fefc326142
Kasia Smolarska <koka.smok7@gmail.com>
2019-11-12 17:10:28 +0100

    passport and Google oAuth implementation

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+require("./services/passport");
 const app = express();
 
 app.get("/", (req, res) => {
@@ -7,6 +7,9 @@ app.get("/", (req, res) => {
     banknotes: "test"
   });
 });
+
+const authRoutes = require("./routes/authRoutes");
+authRoutes(app);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log("listening on port 7000"));
