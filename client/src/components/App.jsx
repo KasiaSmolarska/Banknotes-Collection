ca2750fa4f908b4e708b2802635a2345a91c9355
Kasia Smolarska <koka.smok7@gmail.com>
2019-11-17 20:47:04 +0100

    adding dashboard component -  first setup

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 
 import actions from "../store/actions";
 import Header from "./Header";
 import Landing from "./Landing";
 import LoginPage from "./LoginPage";
+import Dashboard from "./Dashboard";
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/dashboard" component={Dashboard} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   componentDidMount() {
@@ -15,9 +28,10 @@ class App extends Component {
     return (
       <div className="container">
         <BrowserRouter>
-          <Route path="/" render={props => props.location.pathname !== "/login" && <Header />}></Route>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/login" component={LoginPage} />
+          <Switch>
+            <Route exact path="/login" component={LoginPage} />
+            <Route component={Content} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
