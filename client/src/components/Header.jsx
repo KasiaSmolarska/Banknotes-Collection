3531ed6f5e4aa29b6bf30cce894648f7daf4c732
Kasia Smolarska <koka.smok7@gmail.com>
2019-12-28 18:48:06 +0100

    adding dropdown

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,13 +2,30 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import UserIcon from "./svg/UserIcon";
+import LogOutIcon from "./svg/LogOutIcon";
+
 import actions from "../store/actions";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.showMenu = this.showMenu.bind(this);
+  }
   handleClick = e => {
     e.preventDefault();
     this.props.logOutUser();
   };
+
+  state = {
+    showedMenu: false
+  };
+
+  showMenu() {
+    this.setState({
+      showedMenu: !this.state.showedMenu
+    });
+  }
 
   renderContent() {
     switch (this.props.auth) {
@@ -22,9 +39,16 @@ class Header extends Component {
         );
       default:
         return (
-          <button className="btn btn--blue" onClick={this.handleClick}>
-            Log out
-          </button>
+          <div className="nav__account dropdown">
+            <div className="dropdown__button" onClick={this.showMenu}>
+              <UserIcon />
+            </div>
+            <div className={`dropdown__container dropdown__container--left ${this.state.showedMenu && "dropdown__container--visible"}`}>
+              <button className="btn btn--text" onClick={this.handleClick}>
+                <LogOutIcon color="#7a18e3" /> Log out
+              </button>
+            </div>
+          </div>
         );
     }
   }
