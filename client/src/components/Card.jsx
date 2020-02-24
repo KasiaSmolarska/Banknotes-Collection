68accc328b3638a8cbd0372aba3478e0ecb216ce
Kasia Smolarska <koka.smok7@gmail.com>
2020-02-24 13:55:10 +0100

    user tile

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Translate from "../translate/Translate";
 import PropTypes from "prop-types";
 
-export const Card = ({ header, title, children, ...props }) => {
+export const Card = ({ header, title, children, className, mod, ...props }) => {
   return (
-    <div className="card-container" {...props}>
-      <div className="card">
+    <div className={`card-container ${mod ? "card-container--" + mod : ""}`} {...props}>
+      <div className={`card ${className ? className : ""}`}>
         <div className="card__top">
           <div className="truncate">
             <h2>{header && header}</h2>
--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,16 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import { Spinner } from "./Spinner";
 import { BanknotesChart, DefaultChart, getSearchingField } from "./charts/charts";
 import { Card } from "./Card";
-import Translate from "../translate/Translate";
-import PropTypes, { object } from "prop-types";
+import PropTypes from "prop-types";
 
 import actions from "../store/actions";
 
 const getBanknote = state => state.banknote;
+const getUser = state => state.auth.user;
 
 const Dashboard = () => {
   const dispatch = useDispatch();
   const { loading, banknotesList } = useSelector(getBanknote);
+
+  const user = useSelector(getUser);
 
   React.useEffect(() => {
     dispatch(actions.fetchBanknotes());
@@ -21,16 +23,32 @@ const Dashboard = () => {
 
   return !loading ? (
     <div className="dashboard">
-      <Card header={banknotesList.length} title="tile.banknoteAdded">
+      <Card mod="banknote" header={banknotesList.length} title="tile.banknoteAdded">
         <BanknotesChart banknotesList={banknotesList} />
       </Card>
 
-      <Card header={`${Object.keys(getSearchingField(banknotesList, "continent")).length} / 7`} title="tile.continentsAdded">
+      <Card mod="continent" header={`${Object.keys(getSearchingField(banknotesList, "continent")).length} / 7`} title="tile.continentsAdded">
         <DefaultChart banknotesList={banknotesList} value="continent" chartId="continents-chart" seriesName="banknotes" color="#4caf50" />
       </Card>
 
-      <Card header={`${Object.keys(getSearchingField(banknotesList, "country")).length} / 317`} title="tile.countriesAdded">
+      <Card mod="country" header={`${Object.keys(getSearchingField(banknotesList, "country")).length} / 317`} title="tile.countriesAdded">
         <DefaultChart banknotesList={banknotesList} value="country" chartId="countries-chart" seriesName="banknotes" color="#F69F43" />
+      </Card>
+
+      <Card mod="user" className="card--user" header={`Hello ${user.given_name},`}>
+        <img className="card__bg" src="/api/upload/image/14458440-5703-11ea-970c-cd5c3b593a0ddottedBg.png" alt="tile-background" />
+        <div className="card__body">
+          <span>you have already collected banknotes from: </span>
+          <ul className="card__list">
+            <li>
+              <strong>{((Object.keys(getSearchingField(banknotesList, "continent")).length / 7) * 100).toFixed(1)}%</strong> of all continents,
+            </li>
+            <li>
+              <strong>{((Object.keys(getSearchingField(banknotesList, "country")).length / 317) * 100).toFixed(1)}%</strong> of all countries
+            </li>
+          </ul>
+          <strong>Keep collecting & have fun! </strong>
+        </div>
       </Card>
     </div>
   ) : (
