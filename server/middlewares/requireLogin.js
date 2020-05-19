facab973b5e401da9664c5579f6ab3776bc0fcf2
Kasia Smolarska <koka.smok7@gmail.com>
2020-05-19 17:29:06 +0200

    email confirmation fix

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -1,13 +1,10 @@
 module.exports = (req, res, next) => {
-  console.log("req.user",req.user);
 
   if (!req.user) {
-    console.log("req.user",req.user);
     return res.status(401).redirect("/login");
   }
 
   if (!req.user.confirmed) {
-    console.log("req.user",req.user);
     return res.status(401).redirect("/login");
   }
   next();
